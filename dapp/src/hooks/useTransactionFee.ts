c7e9b74b7181912922128dd9f8d84a5737eb7163
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2025-05-10 19:06:07 +0200

    Update dapp to SDK changes
    We updated the `estimateDepositFee` function interface and we need to
    adjust it in the dapp.

--- dapp/src/hooks/useTransactionFee.ts
+++ dapp/src/hooks/useTransactionFee.ts
@@ -1,5 +1,4 @@
 import { useAcreContext } from "#/acre-react/hooks"
-import { Fees as ProtocolFees } from "@acre-btc/sdk"
 import { logPromiseFailure } from "#/utils"
 import { useEffect, useState } from "react"
 import { ACTION_FLOW_TYPES, ActionFlowType, Fee } from "#/types"
@@ -26,26 +25,15 @@ export default function useTransactionFee(
       const getEstimatedDepositFee = async () => {
         if (!acre) return
 
-        let fee: ProtocolFees = {
-          tbtc: initialFee.tbtc.fee,
-          acre: initialFee.acre.fee,
-          total: initialFee.total,
-        }
+        let fee: Fee = initialFee
 
         if (flow === ACTION_FLOW_TYPES.STAKE) {
           fee = await acre.protocol.estimateDepositFee(amount)
         } else if (flow === ACTION_FLOW_TYPES.UNSTAKE) {
-          // TODO: Fetch fees from SDK.
           fee = await acre.protocol.estimateWithdrawalFee(amount)
         }
 
-        const finalFee: Fee = {
-          tbtc: { fee: fee.tbtc, isReimbursable: true },
-          acre: { fee: fee.acre, isReimbursable: false },
-          total: fee.total,
-        }
-
-        setDepositFee(finalFee)
+        setDepositFee(fee)
       }
       logPromiseFailure(getEstimatedDepositFee())
     }
