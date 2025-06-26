127b088533562e1ca7a30818528d72baa69b7e6c
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2025-06-26 09:37:28 +0200

    Simplify the `useTransactionFee` hook
    Use the `useQuery` hook from `@tanstack/react-query` lib and simplify
    the `useTransactionDetails` hook as well.

--- dapp/src/hooks/useTransactionFee.ts
+++ dapp/src/hooks/useTransactionFee.ts
@@ -1,8 +1,7 @@
 import { useAcreContext } from "#/acre-react/hooks"
-import { logPromiseFailure } from "#/utils"
-import { useEffect, useState } from "react"
+import { queryKeysFactory } from "#/constants"
 import { ACTION_FLOW_TYPES, ActionFlowType, Fee } from "#/types"
-import { useAppDispatch } from "./store"
+import { useQuery } from "@tanstack/react-query"
 
 export const initialFee: Fee = {
   tbtc: { fee: 0n, isReimbursable: false },
@@ -13,31 +12,25 @@ export const initialFee: Fee = {
 export default function useTransactionFee(
   amount: bigint | undefined,
   flow: ActionFlowType,
-): Fee {
-  const [depositFee, setDepositFee] = useState<Fee>(initialFee)
+) {
   const { acre } = useAcreContext()
-  const dispatch = useAppDispatch()
 
-  useEffect(() => {
-    if (!amount) {
-      setDepositFee(initialFee)
-    } else {
-      const getEstimatedDepositFee = async () => {
-        if (!acre) return
+  return useQuery({
+    queryKey: [
+      ...queryKeysFactory.userKeys.estimateFee(),
+      flow,
+      amount?.toString(),
+    ],
+    queryFn: () => {
+      if (!acre || !amount) return initialFee
 
-        let fee: Fee = initialFee
-
-        if (flow === ACTION_FLOW_TYPES.STAKE) {
-          fee = await acre.protocol.estimateDepositFee(amount)
-        } else if (flow === ACTION_FLOW_TYPES.UNSTAKE) {
-          fee = await acre.protocol.estimateWithdrawalFee(amount)
-        }
-
-        setDepositFee(fee)
+      if (flow === ACTION_FLOW_TYPES.STAKE) {
+        return acre.protocol.estimateDepositFee(amount)
       }
-      logPromiseFailure(getEstimatedDepositFee())
-    }
-  }, [acre, dispatch, amount, flow])
 
-  return depositFee
+      return acre.protocol.estimateWithdrawalFee(amount)
+    },
+    initialData: initialFee,
+    enabled: !!acre && !!amount,
+  })
 }
