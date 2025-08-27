2002d31b2d12ecd9b595ff224ef7929f3f14b6ed
Jakub Nowakowski <jakub.nowakowski@thesis.co>
2025-08-28 01:19:09 +0200

    Don't use stBTC in MidasAllocator
    MidasAllocator now uses acreBTC instead of stBTC.

--- solidity/contracts/midas/MidasAllocator.sol
+++ solidity/contracts/midas/MidasAllocator.sol
@@ -5,7 +5,6 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Maintainable} from "../utils/Maintainable.sol";
 import {ZeroAddress} from "../utils/Errors.sol";
-import "../stBTC.sol";
 import "../interfaces/IDispatcher.sol";
 import {IVault} from "./IVault.sol";
 
@@ -16,11 +15,11 @@ contract MidasAllocator is IDispatcher, Maintainable {
     /// @notice tBTC token contract.
     IERC20 public tbtc;
 
-    /// @notice stBTC token vault contract.
-    stBTC public stbtc;
+    /// @notice Acre Vault contract (acreBTC).
+    address public acreVault;
 
-    /// @notice Address of the Vault contract.
-    IVault public vault;
+    /// @notice Address of the Midas Vault contract.
+    IVault public midasVault;
 
     /// @notice Address of the VaultReceiptToken contract.
     IERC20 public vaultSharesToken;
@@ -35,30 +34,30 @@ contract MidasAllocator is IDispatcher, Maintainable {
 
     /// @notice Initializes the MidasAllocator contract.
     /// @param _tbtc Address of the tBTC token contract.
-    /// @param _stbtc Address of the stBTC vault contract.
-    /// @param _vault Address of the Midas Vault contract.
+    /// @param _acreVault Address of the Acre Vault contract (acreBTC).
+    /// @param _midasVault Address of the Midas Vault contract.
     function initialize(
         address _tbtc,
-        address _stbtc,
-        address _vault
+        address _acreVault,
+        address _midasVault
     ) public initializer {
         __MaintainableOwnable_init(msg.sender);
 
         if (_tbtc == address(0)) {
             revert ZeroAddress();
         }
-        if (address(_stbtc) == address(0)) {
+        if (address(_acreVault) == address(0)) {
             revert ZeroAddress();
         }
-        if (address(_vault) == address(0)) {
+        if (address(_midasVault) == address(0)) {
             revert ZeroAddress();
         }
 
         tbtc = IERC20(_tbtc);
-        stbtc = stBTC(_stbtc);
-        vault = IVault(_vault);
+        acreVault = _acreVault;
+        midasVault = IVault(_midasVault);
 
-        vaultSharesToken = IERC20(vault.share());
+        vaultSharesToken = IERC20(midasVault.share());
         if (address(vaultSharesToken) == address(0)) {
             revert ZeroAddress();
         }
@@ -67,12 +66,12 @@ contract MidasAllocator is IDispatcher, Maintainable {
     /// @notice Allocate tBTC to the Midas Vault.
     /// @dev This function can be invoked periodically by a maintainer.
     function allocate() external onlyMaintainer {
-        // Fetch unallocated tBTC from stBTC contract.
+        // Fetch unallocated tBTC from Acre Vault contract.
         // slither-disable-next-line arbitrary-send-erc20
         tbtc.safeTransferFrom(
-            address(stbtc),
+            address(acreVault),
             address(this),
-            tbtc.balanceOf(address(stbtc))
+            tbtc.balanceOf(address(acreVault))
         );
 
         // TODO: Revisit when working on queued withdrawals as part of the balance
@@ -80,8 +79,8 @@ contract MidasAllocator is IDispatcher, Maintainable {
         uint256 idleAmount = uint96(tbtc.balanceOf(address(this)));
 
         // Deposit tBTC to Midas Vault.
-        tbtc.forceApprove(address(vault), idleAmount);
-        uint256 shares = vault.deposit(idleAmount, address(this));
+        tbtc.forceApprove(address(midasVault), idleAmount);
+        uint256 shares = midasVault.deposit(idleAmount, address(this));
 
         // slither-disable-next-line reentrancy-events
         emit DepositAllocated(idleAmount, shares);
@@ -96,11 +95,13 @@ contract MidasAllocator is IDispatcher, Maintainable {
     function totalAssets() external view returns (uint256) {
         return
             tbtc.balanceOf(address(this)) +
-            vault.convertToAssets(vaultSharesToken.balanceOf(address(this)));
+            midasVault.convertToAssets(
+                vaultSharesToken.balanceOf(address(this))
+            );
     }
 
     /// @notice Releases deposit in full from Midas Vault and transfers it to the
-    ///         stBTC contract.
+    ///         Acre Vault contract.
     /// @dev This is a special function that can be used to migrate funds during
     ///      allocator upgrade or in case of emergencies.
     function emergencyWithdraw() external onlyOwner {
