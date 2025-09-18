32ada22ef48f9241433cf1b693ccde098a3b41dd
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2025-09-18 21:50:52 +0200

    Add `handleRedeemAndBridgeRequested` handler
    To track the withdrawals. Now in v2, the withdraw id is generated by the
    contract.

--- subgraph/src/withdrawal-queue.ts
+++ subgraph/src/withdrawal-queue.ts
@@ -1,25 +1,34 @@
-import { log } from "@graphprotocol/graph-ts"
-import { Withdraw } from "../generated/schema"
 import { RedeemAndBridgeRequested } from "../generated/WithdrawalQueue/WithdrawalQueue"
+import {
+  getOrCreateDepositOwner,
+  getOrCreateEvent,
+  getOrCreateWithdraw,
+} from "./utils"
 
 // This event is emitted in the same transaction as the `RedemptionRequested`
-// event from the `BitcoinRedeemerV2` contract. We need to set the
-// requestedAmount` field.
+// event from the `BitcoinRedeemerV2` contract.
 // eslint-disable-next-line import/prefer-default-export
 export function handleRedeemAndBridgeRequested(
   event: RedeemAndBridgeRequested,
 ): void {
-  const withdrawId = event.params.requestId.toString()
-  const withdraw = Withdraw.load(withdrawId)
+  const ownerEntity = getOrCreateDepositOwner(event.params.redeemer)
 
-  if (!withdraw) {
-    log.error("Withdraw entity not found. Withdraw id: {}", [withdrawId])
-    return
-  }
+  const withdraw = getOrCreateWithdraw(event.params.requestId.toString())
 
+  withdraw.depositOwner = ownerEntity.id
   withdraw.requestedAmount = event.params.tbtcAmount.plus(
     event.params.exitFeeInTbtc,
   )
 
+  const redemptionRequestedEvent = getOrCreateEvent(
+    `${event.transaction.hash.toHexString()}_RedeemAndBridgeRequested`,
+  )
+
+  redemptionRequestedEvent.activity = withdraw.id
+  redemptionRequestedEvent.timestamp = event.block.timestamp
+  redemptionRequestedEvent.type = "Requested"
+
+  ownerEntity.save()
   withdraw.save()
+  redemptionRequestedEvent.save()
 }
